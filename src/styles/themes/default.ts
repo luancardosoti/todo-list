451411770c63ba95c621d34d40d2d47fb1679e90
Luan Cardoso <luancardosoti@gmail.com>
2022-10-29 12:09:39 -0300

    adicionando estilo global, fonte e tema da aplicação

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  'purple-500': '#8284FA',
+  'purple-700': '#5E60CE',
+
+  'blue-500': '#4EA8DE',
+  'blue-700': '#1E6F9F',
+
+  'gray-100': '#f2f2f2',
+  'gray-200': '#d9d9d9',
+  'gray-300': '#808080',
+  'gray-400': '#333333',
+  'gray-500': '#262626',
+  'gray-600': '#1a1a1a',
+  'gray-700': '#0d0d0d',
+
+  'red-500': '#e25858',
+}
