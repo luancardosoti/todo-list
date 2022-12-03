836841339a5363b4b3776f21d6111984286dc963
Luan Cardoso <luancardosoti@gmail.com>
2022-12-03 13:28:22 -0300

    build v1

--- src/components/Header/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const HeaderContainer = styled.header`
-  background: ${(props) => props.theme.gray700};
-  text-align: center;
-  padding-top: 4.5rem;
-  padding-bottom: 5rem;
-`
